3bf3d0aceadbd1865575a7b6e598be2ce4e85535
Adriaan <adriaanmarain300@gmail.com>
2020-02-19 17:39:03 +0100

    wip renderless components

--- resources/js/vue/Index.vue
+++ resources/js/vue/Index.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>kek</div>
+    <div>
+        <p>test</p>
+        <medialibrary>
+            <div slot-scope="props">--- {{ props }} ---</div>
+        </medialibrary>
+    </div>
 </template>
 
 <script>
+import Medialibrary from "@spatie/medialibrary-pro-vue";
+
 export default {
-
-}
+    components: { Medialibrary }
+};
 </script>
-
-<style>
-
-</style>
