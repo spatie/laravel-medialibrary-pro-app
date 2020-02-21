2664099822d3c9620d9fddc90d8442b7b700819e
Adriaan <adriaanmarain300@gmail.com>
2020-02-21 10:46:41 +0100

    single-upload in form

--- resources/js/vue/SingleUpload.vue
+++ resources/js/vue/SingleUpload.vue
@@ -1,36 +1,60 @@
 <template>
     <div>
-        <medialibrary :endpoint="endpoint">
-            <template slot-scope="{ mediaLibrary }">
+        <form :action="endpoint" method="POST">
+            <slot />
+
+            <media-library :endpoint="tempEndpoint" @loaded="setMediaLibrary">
+                <template slot-scope="{ mediaLibrary }">
+                    <input
+                        type="file"
+                        :accept="accept"
+                        :multiple="multiple"
+                        @change="e => handleUpload(e, mediaLibrary)"
+                    />
+                </template>
+            </media-library>
+
+            <div>
                 <input
-                    type="file"
-                    :accept="accept"
-                    :multiple="multiple"
-                    @change="e => handleUpload(e, mediaLibrary)"
+                    type="hidden"
+                    name="file_name"
+                    :value="JSON.stringify(value)"
                 />
-            </template>
-        </medialibrary>
+            </div>
+
+            <button>Submit</button>
+        </form>
     </div>
 </template>
 
 <script>
-import Medialibrary from "@spatie/medialibrary-pro-vue";
+import MediaLibrary from "@spatie/medialibrary-pro-vue";
 
 export default {
     props: {
+        tempEndpoint: { required: true, type: String },
         endpoint: { required: true, type: String }
     },
 
-    components: { Medialibrary },
+    components: { MediaLibrary },
 
-    data() {
-        return {
-            accept: "",
-            multiple: true
-        };
+    data: () => ({
+        accept: "",
+        multiple: true,
+        mediaLibrary: null
+    }),
+
+    computed: {
+        value() {
+            return this.mediaLibrary ? this.mediaLibrary.state.media : [];
+        }
     },
 
     methods: {
+        setMediaLibrary(mediaLibrary) {
+            this.mediaLibrary = mediaLibrary;
+        },
+
         handleUpload(changeEvent, mediaLibrary) {
             Array.from(changeEvent.target.files).forEach(file =>
                 mediaLibrary.addFile(file)
