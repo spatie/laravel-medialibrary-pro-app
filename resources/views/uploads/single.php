85c43b43ef5c149fc08c5e771382c1d805709976
Adriaan <adriaanmarain300@gmail.com>
2020-02-21 09:51:24 +0100

    use correct endpoint

--- resources/views/uploads/single.php
+++ resources/views/uploads/single.php
@@ -68,7 +68,7 @@
     <body>
         <div class="flex-center position-ref full-height">
             <div id="app">
-                <single-upload endpoint="temp_upload" />
+                <single-upload endpoint="temp-upload" />
             </div>
         </div>
     </body>
