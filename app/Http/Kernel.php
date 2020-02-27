6d960d19d6a6a9e11f5b7ac8163cd2abbd753d6a
freek <freek@spatie.be>
2020-02-27 18:39:26 +0100

    wip

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,7 +29,6 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            FakeUser::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -37,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            FakeUser::class,
         ],
 
         'api' => [
