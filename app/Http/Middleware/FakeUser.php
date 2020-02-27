a37c74b4321e22eb753923e51c580eef92a095ac
freek <freek@spatie.be>
2020-02-27 18:42:23 +0100

    wip

--- app/Http/Middleware/FakeUser.php
+++ app/Http/Middleware/FakeUser.php
@@ -16,7 +16,10 @@ class FakeUser
      */
     public function handle($request, Closure $next)
     {
-        auth()->login(factory(User::class)->make());
+        auth()->login(User::make([
+            'name' => 'Dummy',
+            'email' => 'john@example.com',
+        ]));
 
         return $next($request);
     }
