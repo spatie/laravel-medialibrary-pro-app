10d3882c2c6e7c43478ce4138893c1aebb05dc0b
freek <freek@spatie.be>
2020-02-28 00:41:13 +0100

    wip

---
+++ app/Console/Commands/HandleAwsEventCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class HandleAwsEventCommand extends Command
+{
+    protected $signature = 'vapor:handle {payload}';
+
+    protected $description = 'Handle AWS event';
+
+    public function handle()
+    {
+        $payload = json_decode(
+            base64_decode($this->argument('payload'))
+        );
+
+        info(print_r($payload, true));
+    }
+}
