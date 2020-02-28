a78e8474eebd56fc2dad6b0d18d0e8049a4f76e0
freek <freek@spatie.be>
2020-02-28 11:21:14 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use App\Http\Controllers\FormSubmissionController;
+use Spatie\MedialibraryPro\Http\Controllers\CreateTemporaryUploadFromDirectS3UploadController;
 use Spatie\MedialibraryPro\Http\Controllers\UploadController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-
 Route::get('multi-vue', [FormSubmissionController::class, 'showVue']);
 Route::get('multi-react', [FormSubmissionController::class, 'showReact']);
 
 Route::post('temp-upload', UploadController::class);
+Route::post('post-s3-upload', CreateTemporaryUploadFromDirectS3UploadController::class);
 
 Route::post('multi-upload', [FormSubmissionController::class, 'store']);
 
