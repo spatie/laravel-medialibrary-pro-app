34fbd780c5e3e4960d97ac63523c59edba2897be
freek <freek@spatie.be>
2020-03-26 10:38:17 +0100

    wip

--- app/Http/Requests/StoreMultipleUploadsRequest.php
+++ app/Http/Requests/StoreMultipleUploadsRequest.php
@@ -6,19 +6,22 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreMultipleUploadsRequest extends FormRequest
 {
+    public function fieldName()
+    {
+        return 'single-media';
+    }
+
     public function rules()
     {
         return [
-            'name' => 'required',
-            'media' => 'required|array',
-            'media.*.uuid' => 'required|uuid',
-            'media.*.order' => 'required|numeric',
-            'media.*.name' => 'required|string',
-            'media.*.thumbnail' => 'required|string',
-            'media.*.custom_properties' => 'required|array',
+            "{$this->fieldName()}.*.uuid" => 'required|uuid',
+            "{$this->fieldName()}.*.order" => "required|numeric",
+            "{$this->fieldName()}.*.name" => "required|string",
+            "{$this->fieldName()}.*.thumbnail" => "required|string",
+            //"{$this->fieldName()}.*.custom_properties" => "required|array",
             /* Custom validation rules. These should probably be configurable by the user from somewhere else */
-            'media.*.custom_properties.tags' => 'required',
-            'media.*.custom_properties.caption' => 'required',
+           // "{$this->fieldName()}.*.custom_properties.tags" => "required",
+           // "{$this->fieldName()}.*.custom_properties.caption" => "required",
         ];
     }
 }
