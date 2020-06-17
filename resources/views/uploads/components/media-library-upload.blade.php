2f45269f348718889c77b8e7ce23db1659fa9d10
freek <freek@spatie.be>
2020-06-17 21:47:47 +0200

    wip

--- resources/views/uploads/components/media-library-upload.blade.php
+++
@@ -1,7 +0,0 @@
-<livewire:upload-single
-    :form-element-name="$name"
-    :rules="$rules"
-    :upload-name="$oldValues['name'] ?? ''"
-    :preview-url="$oldValues['preview'] ?? ''"
-    :media-uuid="$oldValues['uuid'] ?? ''"
-/>
