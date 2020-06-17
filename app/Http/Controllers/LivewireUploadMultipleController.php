a4b9a221f03238d25771ba20f80225a143d4be78
freek <freek@spatie.be>
2020-06-17 22:04:21 +0200

    wip

--- app/Http/Controllers/LivewireUploadMultipleController.php
+++ app/Http/Controllers/LivewireUploadMultipleController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Concerns\StoresFormSubmissions;
 use App\Http\Requests\StoreMultipleUploadsRequest;
 use App\Models\FormSubmission;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class LivewireUploadMultipleController
 {
     public function create()
     {
         /** @var \App\Models\FormSubmission $formSubmission */
-        $formSubmission = FormSubmission::firstOrCreate(['name' => 'livewire multiple']);
+        $formSubmission = FormSubmission::firstOrCreate([]);
 
         return view('uploads.livewire.multiple', compact('formSubmission'));
     }
@@ -20,9 +18,14 @@ class LivewireUploadMultipleController
     public function store(StoreMultipleUploadsRequest $request)
     {
         /** @var \App\Models\FormSubmission $formSubmission */
-        $formSubmission = FormSubmission::whereName('livewire multiple')->first()
+        $formSubmission = FormSubmission::first();
+
+        $formSubmission
             ->addFromMediaLibraryRequest('media', $request)
             ->each->toMediaCollection('images');
+
+        $formSubmission->name = $request->name;
+        $formSubmission->save();
 
         flash()->success('Your form has been submitted');
 
