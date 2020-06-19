43cfeae16cc2e3c060d1be789f53ca5259e92e18
Adriaan <adriaanmarain300@gmail.com>
2020-06-19 17:22:54 +0200

    different types for values

--- resources/js/react/module.d.ts
+++ resources/js/react/module.d.ts
@@ -1,12 +1,10 @@
+type MediaAttributes = import('../../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-core/src/types').MediaLibrary.MediaAttributes;
+
 interface Window {
     oldValues: {
         name: string;
-        singleMedia: ReadonlyArray<
-            import('../../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-core/src/types').MediaLibrary.MediaAttributes
-        >;
-        media: ReadonlyArray<
-            import('../../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-core/src/types').MediaLibrary.MediaAttributes
-        >;
+        singleMedia: ReadonlyArray<MediaAttributes>;
+        media: ReadonlyArray<MediaAttributes> | { [uuid: string]: MediaAttributes };
     };
     errors: { [key: string]: Array<string> };
     tempEndpoint: string;
