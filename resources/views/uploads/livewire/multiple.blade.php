793b9461bcbe0584ff92a54c3d020c408bc22e86
freek <freek@spatie.be>
2020-06-22 18:24:22 +0200

    wip

--- resources/views/uploads/livewire/multiple.blade.php
+++ resources/views/uploads/livewire/multiple.blade.php
@@ -7,6 +7,7 @@
 
         Name: <input type="text" name="name" value="{{ old('name', $formSubmission->name) }}">
 
+        {{--
         <h1 class="text-2xl">Images</h1>
 
         <x-media-library-collection
@@ -15,6 +16,7 @@
             collectionName="images"
             collection-rules="max:3"
         />
+        --}}
 
         <h1 class="text-2xl">Downloads</h1>
 
@@ -24,6 +26,7 @@
             :model="$formSubmission"
             collectionName="downloads"
             collection-rules="max:3"
+            view="uploads.livewire.partials"
         />
 
         <button type="submit">Submit</button>
