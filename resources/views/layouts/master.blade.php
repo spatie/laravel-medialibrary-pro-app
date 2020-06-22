534f477153f2511897e9ff606850b70098b47733
Adriaan <adriaanmarain300@gmail.com>
2020-06-22 12:33:34 +0200

    sortable livewire components

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -41,4 +41,26 @@
 </div>
 </body>
 <livewire:scripts />
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/dragula/3.7.2/dragula.min.js"></script>
+<style>.gu-mirror{position:fixed!important;margin:0!important;z-index:9999!important;opacity:.8;-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=80)";filter:alpha(opacity=80)}.gu-hide{display:none!important}.gu-unselectable{-webkit-user-select:none!important;-moz-user-select:none!important;-ms-user-select:none!important;user-select:none!important}.gu-transit{opacity:.2;-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=20)";filter:alpha(opacity=20)}</style>
+
+<script>
+    dragula(querySelectorAllArray('.dragula-container'), {
+        moves (el, source, handle) {
+            if (!handle) {
+                    return false;
+                }
+
+            return Boolean(handle.closest('.dragula-handle'));
+        }
+    });
+
+
+    function querySelectorAllArray(selector){
+        return Array.prototype.slice.call(
+            document.querySelectorAll(selector), 0
+        );
+    }
+</script>
 </html>
