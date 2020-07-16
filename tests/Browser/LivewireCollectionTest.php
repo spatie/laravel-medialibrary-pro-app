bd7969592f329437d0121d12c29752dc84b470b9
freek <freek@spatie.be>
2020-07-16 14:39:08 +0200

    wip

--- tests/Browser/LivewireCollectionTest.php
+++ tests/Browser/LivewireCollectionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Models\FormSubmission;
 use Laravel\Dusk\Browser;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Tests\DuskTestCase;
@@ -15,14 +16,32 @@ class LivewireCollectionTest extends DuskTestCase
             $browser
                 ->visit('/livewire/collection')
                 ->attach('[data-testing-role="main-uploader"]', $this->getStubPath('space.png'))
-
                 ->waitUntilMissingText('Uploading')
                 ->press('[data-testing-role="submit"]')
                 ->assertSee('Your form has been submitted')
                 ->assertVisible('[data-testing-role="thumb"]');
 
-            $this->assertCount(1, Media::all());
+            $this->assertCount(1, FormSubmission::getTestModel()->getMedia('images'));
         });
+    }
 
+    /** @test */
+    public function uploading_a_file_and_removing_it_before_submitting_the_form_will_not_attach_media_to_the_model()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser
+                ->visit('/livewire/collection')
+                ->attach('[data-testing-role="main-uploader"]', $this->getStubPath('space.png'))
+                ->waitUntilMissingText('Uploading')
+
+                ->assertVisible('[data-testing-role="thumb"]')
+                ->press('[data-testing-role="remove"]')
+
+                ->assertMissing('[data-testing-role="thumb"]')
+                ->assertSee('Your form has been submitted')
+                ->assertMissing('[data-testing-role="thumb"]');
+
+            $this->assertCount(1, FormSubmission::getTestModel()->getMedia('images'));
+        });
     }
 }
