0b87eda13e691dd8ba0f3ffd026e65f82f2d87ec
Willem Van Bockstal <willem@spatie.be>
2020-07-27 11:10:20 +0200

    Liverwire multiple attachments

--- resources/views/uploads/livewire/attachment-multiple.blade.php
+++ resources/views/uploads/livewire/attachment-multiple.blade.php
@@ -2,16 +2,20 @@
 
 @section('content')
 
-    <div>
-        <form method="POST">
-            @csrf
+    <x-h2>Livewire: attachment (multiple)</x-h2>
 
-            <input id="name" name="name" placeholder="Your first name">
+    <x-form method="POST">
+        @csrf
 
+        <x-field label="name">
+            <x-input id="name" name="name" placeholder="Your first name" />
+        </x-field>
+
+        <x-field label="files">
             <x-media-library-attachment multiple name="media" rules="mimes:png,jpg,pdf" />
+        </x-field>
 
-            <button data-testing-role="submit" type="submit">Submit</button>
-        </form>
-    </div>
+        <x-button data-testing-role="submit" type="submit">Submit</x-button>
+    </x-form>
 
 @endsection
