01f12e849474cebf7167bc7ab4c77e2639104e4e
Willem Van Bockstal <willem@spatie.be>
2020-09-02 15:55:28 +0200

    AsyncAttachment in Vue

--- resources/js/vue/app.js
+++ resources/js/vue/app.js
@@ -2,13 +2,13 @@ require('../bootstrap');
 
 import Vue from 'vue';
 
-import AsyncSingle from './AsyncSingle.vue';
+import AsyncAttachment from './AsyncAttachment.vue';
 
 import MediaLibraryAttachment from '../../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-vue-attachment';
 import MediaLibraryCollection from '../../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-vue-collection';
 
 var app = new Vue({
-    components: { AsyncSingle, MediaLibraryAttachment, MediaLibraryCollection },
+    components: { AsyncAttachment, MediaLibraryAttachment, MediaLibraryCollection },
 
     el: '#app',
 
