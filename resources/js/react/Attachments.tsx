47d7fd8b26743537b4accf146aa69b913127ad1e
Adriaan <adriaanmarain300@gmail.com>
2020-09-11 11:00:34 +0200

    react attachments component update

--- resources/js/react/Attachments.tsx
+++ resources/js/react/Attachments.tsx
@@ -5,14 +5,19 @@ import Field from './components/Field';
 import Grid from './components/Grid';
 
 export default function Attachments() {
-
     return (
         <>
-            <H2>React: multiple attachment</H2>
+            <H2>React: multiple attachment (max 3 items)</H2>
 
             <Grid>
                 <Field label="files">
-                    <MediaLibraryAttachment name="media" uploadEndpoint="/temp-upload" multiple />
+                    <MediaLibraryAttachment
+                        name="media"
+                        uploadEndpoint="/temp-upload"
+                        validation={{ accept: ['image/png'], maxSize: 2048 }}
+                        multiple
+                        maxItems={3}
+                    />
                 </Field>
             </Grid>
         </>
