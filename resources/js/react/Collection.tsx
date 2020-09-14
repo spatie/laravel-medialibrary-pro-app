5fb66b74c8c0eb08c3676c56a20c14f820da88da
Adriaan <adriaanmarain300@gmail.com>
2020-09-14 12:33:55 +0200

    prettier

--- resources/js/react/Collection.tsx
+++ resources/js/react/Collection.tsx
@@ -16,80 +16,77 @@ export default function Collection() {
 
             <form method="POST">
                 <Grid>
-                    <Csrf token={window.csrfToken}/>
+                    <Csrf token={window.csrfToken} />
 
                     <Field label="Name">
-                        <Input
-                            name="name"
-                            type="text"
-                            placeholder="name"
-                            defaultValue={window.oldValues.name}
-                        />
+                        <Input name="name" type="text" placeholder="name" defaultValue={window.oldValues.name} />
                     </Field>
 
                     <Field label="Files">
                         <MediaLibraryCollection
-                        name="media"
-                        initialValue={window.oldValues.media}
-                        uploadEndpoint={window.uploadEndpoint}
-                        translations={{
-                            hint: { plural: 'Add some files!', singular: 'Add a file!' },
-                            replace: 'drag or click to replace',
-                        }}
-                        validation={{ accept: ['image/png'], maxSize: 1048576 }}
-                        validationErrors={window.errors}
-                        sortable
-                        beforeUpload={() => new Promise(resolve => resolve())}
-                        onIsReadyToSubmitChange={setIsReadyToSubmit}
-                        afterItems={({
-                            getCustomPropertyInputProps,
-                            getCustomPropertyInputErrors,
-                            getNameInputProps,
-                            getNameInputErrors,
-                        }) => (
-                            <>
-                                <div className="mb-2">
-                                    <input className="border rounded" placeholder="image name" {...getNameInputProps()} />
-                                    {getNameInputErrors().map(error => (
-                                        <p key={error} className="text-red-500">
-                                            {error}
-                                        </p>
-                                    ))}
-                                </div>
+                            name="media"
+                            initialValue={window.oldValues.media}
+                            uploadEndpoint={window.uploadEndpoint}
+                            translations={{
+                                hint: { plural: 'Add some files!', singular: 'Add a file!' },
+                                replace: 'drag or click to replace',
+                            }}
+                            validation={{ accept: ['image/png'], maxSize: 1048576 }}
+                            validationErrors={window.errors}
+                            sortable
+                            beforeUpload={() => new Promise(resolve => resolve())}
+                            onIsReadyToSubmitChange={setIsReadyToSubmit}
+                            afterItems={({
+                                getCustomPropertyInputProps,
+                                getCustomPropertyInputErrors,
+                                getNameInputProps,
+                                getNameInputErrors,
+                            }) => (
+                                <>
+                                    <div className="mb-2">
+                                        <input
+                                            className="border rounded"
+                                            placeholder="image name"
+                                            {...getNameInputProps()}
+                                        />
+                                        {getNameInputErrors().map(error => (
+                                            <p key={error} className="text-red-500">
+                                                {error}
+                                            </p>
+                                        ))}
+                                    </div>
 
-                                <div className="mb-2">
-                                    <input
-                                        className="border rounded"
-                                        placeholder="tags"
-                                        {...getCustomPropertyInputProps('tags')}
-                                    />
-                                    {getCustomPropertyInputErrors('tags').map(error => (
-                                        <p key={error} className="text-red-500">
-                                            {error}
-                                        </p>
-                                    ))}
-                                </div>
+                                    <div className="mb-2">
+                                        <input
+                                            className="border rounded"
+                                            placeholder="tags"
+                                            {...getCustomPropertyInputProps('tags')}
+                                        />
+                                        {getCustomPropertyInputErrors('tags').map(error => (
+                                            <p key={error} className="text-red-500">
+                                                {error}
+                                            </p>
+                                        ))}
+                                    </div>
 
-                                <div className="mb-2">
-                                    <input
-                                        className="border rounded"
-                                        placeholder="caption"
-                                        {...getCustomPropertyInputProps('caption')}
-                                    />
-                                    {getCustomPropertyInputErrors('caption').map(error => (
-                                        <p key={error} className="text-red-500">
-                                            {error}
-                                        </p>
-                                    ))}
-                                </div>
-                            </>
-                        )}
-                    ></MediaLibraryCollection>
+                                    <div className="mb-2">
+                                        <input
+                                            className="border rounded"
+                                            placeholder="caption"
+                                            {...getCustomPropertyInputProps('caption')}
+                                        />
+                                        {getCustomPropertyInputErrors('caption').map(error => (
+                                            <p key={error} className="text-red-500">
+                                                {error}
+                                            </p>
+                                        ))}
+                                    </div>
+                                </>
+                            )}
+                        ></MediaLibraryCollection>
                     </Field>
 
-                    <Button disabled={!isReadyToSubmit}>
-                        Submit
-                    </Button>
+                    <Button disabled={!isReadyToSubmit}>Submit</Button>
                 </Grid>
             </form>
         </>
