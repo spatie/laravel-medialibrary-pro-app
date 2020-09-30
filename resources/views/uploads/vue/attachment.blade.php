7e44cf9f0506393b0aeeee4a5a5b1b7f4027bb40
Adriaan <adriaanmarain300@gmail.com>
2020-09-30 16:25:48 +0200

    clean up vue attachment component view

--- resources/views/uploads/vue/attachment.blade.php
+++ resources/views/uploads/vue/attachment.blade.php
@@ -12,6 +12,10 @@
     @csrf
 
     <x-grid>
+        <x-field label="name">
+            <x-input id="name" name="name" placeholder="Your first name" />
+        </x-field>
+
         <x-field label="file">
             <media-library-attachment
                 name="media"
@@ -19,8 +23,12 @@
                 :validation="{ accept: ['image/png'], maxSize: 1048576 }"
             />
         </x-field>
+        
+        @error('media')
+            <p class="rounded-sm text-xs mb-8 px-4 py-2 bg-red-100 text-red-500">{{ $message }}</p>
+        @enderror
 
-        <button type="submit">submit</button>
+        <x-button data-testing-role="submit">Submit</x-button>
     </x-grid>
 </form>
 
