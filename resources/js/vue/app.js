19810b090f5ee2d7c208ba065fbb238d83418b28
freek <freek@spatie.be>
2020-10-07 17:30:21 +0200

    wip

--- resources/js/vue/app.js
+++ resources/js/vue/app.js
@@ -7,7 +7,7 @@ import AsyncAttachments from './AsyncAttachments.vue';
 import MediaLibraryAttachment from '../../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-vue-attachment/dist';
 import MediaLibraryCollection from '../../../vendor/spatie/laravel-medialibrary-pro/ui/medialibrary-pro-vue-collection/dist';
 
-var app = new Vue({
+new Vue({
     components: { AsyncAttachments, MediaLibraryAttachment, MediaLibraryCollection },
 
     el: '#app',
