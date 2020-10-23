a2a19cfc40e7cd82c9f86c333552644fa4d8d71f
freek <freek@spatie.be>
2020-10-23 08:33:30 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        ld('in app service provider');
         Flash::levels([
             'success' => 'success',
             'error' => 'error',
